4f3c099f07b323051fe35219192670d50a201c86
Shahed Talukder <shahedtalukder51@gmail.com>
2020-04-15 17:06:53 +0600

    hell

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+# different type of printing methods
 print("Hello World")
+print('Hello world')
+print("Hello 'World'")
+print('Hello "World"')
+print("Hello \"World")
+print('Hello \'World')
+print("This is backslash\\")
+print("Shahed \\ Talukder")
+# For example
+print("Line A \n Line B")
+# hard way to print \n or other escape sequence
+print("Line A \\n Line B")
+print("\\\" \\'")
+# Remember that
+# \' gives '
+#\\ gives \
+# and for this
+print("\\\" \\'")
+# Exercise
+print("this is \\\\ double backslash")
+print("This is /\\/\\/\\/\\/\\ mountains")
+print("he is \tawesome")
+print("\\\" \\n \\t \\\'")
+#shortcut way to print \
+print(r"Hello \n World")
+#Exercise solve with shortcut way
+print(r"this is \\ double backslash")
+print(r"this are /\/\/\/\/\/\ mountains")
+print("he is \tawesome")
+print(r"\" \n \t \'")
+#Emoji printing
+#print("U+1F602") previous form
+print("\U0001F602\U0001F602\U0001F602\U0001F602")  # next form
