2f13c3bfcbcae75c06be513624d49ea5e3c8e642
Shahed Talukder <shahedtalukder51@gmail.com>
2020-04-20 22:31:10 +0600

    hello

--- file/csv_write.py
+++ file/csv_write.py
@@ -1,67 +1,67 @@
-# from csv import writer,DictWriter
+from csv import writer,DictWriter
 
-# with open('file.csv','a', newline='') as f:
-#     csv_writer = writer(f)
-#     # methods -- writerow, writerows
-#     # writerow
-#     # csv_writer.writerow(['name','country'])
-#     # csv_writer.writerow(['shahed','bd'])
-#     # csv_writer.writerow(['azad','bd'])
-#     # csv_writer.writerow(['ashik','bd'])
-#     # csv_writer.writerow(['rajib','bd'])
-#     # csv_writer.writerow(['atik','bd'])
-#     # writerows
-#     csv_writer.writerows([
-#         ['name','countries'],
-#         ['Shahed','bd'],
-#         ['Ashik','bd'],
-#         ['Azad','bd'],
-#         ['Rajib','bd'],
-#         ['Atik','bd']
-#     ])
-# # import python_ascii
-# # print(python_ascii.__package__)
+with open('file.csv','a', newline='') as f:
+    csv_writer = writer(f)
+    # methods -- writerow, writerows
+    # writerow
+    # csv_writer.writerow(['name','country'])
+    # csv_writer.writerow(['shahed','bd'])
+    # csv_writer.writerow(['azad','bd'])
+    # csv_writer.writerow(['ashik','bd'])
+    # csv_writer.writerow(['rajib','bd'])
+    # csv_writer.writerow(['atik','bd'])
+    # writerows
+    csv_writer.writerows([
+        ['name','countries'],
+        ['Shahed','bd'],
+        ['Ashik','bd'],
+        ['Azad','bd'],
+        ['Rajib','bd'],
+        ['Atik','bd']
+    ])
+# import python_ascii
+# print(python_ascii.__package__)
 
-# with open('final.csv','w',newline='') as f:
-#     csv_writer = DictWriter(f, fieldnames=['first_name','last_name','age'])
-#     csv_writer.writeheader()
-#     # csv_writer.writerow({
-#     #     'first_name':'Shahed',
-#     #     'last_name':'Talukder',
-#     #     'age':'22'
-#     # })
-#     # csv_writer.writerow({
-#     #     'first_name':'Ashik',
-#     #     'last_name':'Mia',
-#     #     'age':'22'
-#     # })
-#     # csv_writer.writerow({
-#     #     'first_name':'Azad',
-#     #     'last_name':'Mia',
-#     #     'age':'22'
-#     # })
-#     csv_writer.writerows([
-#         {
-#             'first_name':'Shahed',
-#             'last_name':'Talukder',
-#             'age':'22'
-#         },
-#         {
-#             'first_name':'Ashik',
-#             'last_name':'Mia',
-#             'age':'22'
-#         },
-#         {
-#             'first_name':'Azad',
-#             'last_name':'Mia',
-#             'age':'22'
-#         },
-#         {
-#             'first_name':'Rajib',
-#             'last_name':'Mia',
-#             'age':'22'
-#         }
-#     ])
+with open('final.csv','w',newline='') as f:
+    csv_writer = DictWriter(f, fieldnames=['first_name','last_name','age'])
+    csv_writer.writeheader()
+    # csv_writer.writerow({
+    #     'first_name':'Shahed',
+    #     'last_name':'Talukder',
+    #     'age':'22'
+    # })
+    # csv_writer.writerow({
+    #     'first_name':'Ashik',
+    #     'last_name':'Mia',
+    #     'age':'22'
+    # })
+    # csv_writer.writerow({
+    #     'first_name':'Azad',
+    #     'last_name':'Mia',
+    #     'age':'22'
+    # })
+    csv_writer.writerows([
+        {
+            'first_name':'Shahed',
+            'last_name':'Talukder',
+            'age':'22'
+        },
+        {
+            'first_name':'Ashik',
+            'last_name':'Mia',
+            'age':'22'
+        },
+        {
+            'first_name':'Azad',
+            'last_name':'Mia',
+            'age':'22'
+        },
+        {
+            'first_name':'Rajib',
+            'last_name':'Mia',
+            'age':'22'
+        }
+    ])
 
 
 from csv import DictReader,DictWriter
@@ -82,3 +82,18 @@ with open('final.csv','r') as rf:
                 'last_name':lname.upper(),
                 'salary': salary
             })
+import os
+import time
+f = os.getcwd()
+# os.mkdir('5555')
+for i in range(1,10000):
+    # os.mkdir(str(i)+"take")
+    # time.sleep(-50)
+    os.rmdir(str(i)+"take")
+# os.rmdir('5555')
+# if os.path.exists()
+
+# create file
+open('file.txt','a').close()
+
+os.system('o')
---
+++ file/os_module.py
@@ -0,0 +1,31 @@
+import os
+import time
+# f = os.getcwd()
+# # os.mkdir('5555')
+# for i in range(1,10000):
+#     # os.mkdir(str(i)+"take")
+#     # time.sleep(-50)
+#     os.rmdir(str(i)+"take")
+# # os.rmdir('5555')
+# # if os.path.exists()
+
+# # create file
+# open('file.txt','a').close()
+
+# command = f"touch shahed_{1}.txt"
+# os.system(command)
+
+# for i in range(1,100):
+#     command = f"touch shahed_{i}.txt"
+#     os.system(command)
+
+# time.sleep(50)
+
+# for i in range(1,100):
+#     command = f"rm shahed_{i}.txt"
+#     os.system(command)
+
+website = input("Enter your website name:")
+
+command = f"ping {website}"
+os.system(command)
