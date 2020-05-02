f58dee51fa00d31f8053c3be126204847adb6417
Shahed Talukder <shahedtalukder51@gmail.com>
2020-05-02 16:50:42 +0600

    hello

--- google_challange/the-grandest-staircase-of-them-all.py
+++ google_challange/the-grandest-staircase-of-them-all.py
@@ -3,7 +3,7 @@ def formulas(n):
     size = n + 1
     #create zero-filled matrix
     matrix = [[0 for _ in xrange(size)] for _ in xrange(size)]
-    
+
     #base value is always padded and skipped
     matrix[0][0] = 1
     for i in xrange(1, size):
@@ -13,8 +13,7 @@ def formulas(n):
                 matrix[i][j] += matrix[i - 1][j - i]
     return matrix[n][n] - 1
 
+
 def solution(n):
     # Your code here
     return formulas(n)
-
-    
